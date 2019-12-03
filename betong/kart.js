94cc3f246bd69e6421bc0cecefe4e646f235a505
martbry <martin_b91@hotmail.com>
2019-12-03 12:27:19 +0100

    Kartknapper

--- betong/kart.js
+++ betong/kart.js
@@ -1,5 +1,7 @@
 let mainhtml = document.getElementById("mainContent");
 let knappebredde = 90;
+let knappehoyde = 58;
+let bildehoyde = 505;
 
 function kartside(){
     let html = "";
@@ -7,11 +9,11 @@ function kartside(){
     html += 
     `<div id="kartcontainer">
     <input type="text" style="position: absolute; width: 340px; margin-left: 10px;" value="Søk:"></input>
-    <img src="kart.png" style="width: 360px; height: 505px;"></img>
-    <button style="width: ${knappebredde}px; display: inline-block; position: absolute; top: 492px; left: ${7+(0*knappebredde)}px;">0</button>
-    <button style="width: ${knappebredde}px; display: inline-block; position: absolute; top: 492px; left: ${7+(1*knappebredde)}px;">1</button>
-    <button style="width: ${knappebredde}px; display: inline-block; position: absolute; top: 492px; left: ${7+(2*knappebredde)}px;">2</button>
-    <button style="width: ${knappebredde}px; display: inline-block; position: absolute; top: 492px; left: ${7+(3*knappebredde)}px;">3</button>
+    <img src="kart.png" style="width: 360px; height: ${bildehoyde}px;"></img>
+    <button class="kartknapper" style="height: ${knappehoyde}px; width: ${knappebredde}px; display: inline-block; position: absolute; top: ${bildehoyde-knappehoyde}px; left: ${(0*knappebredde)}px;">0</button>
+    <button class="kartknapper" style="height: ${knappehoyde}px; width: ${knappebredde}px; display: inline-block; position: absolute; top: ${bildehoyde-knappehoyde}px; left: ${(1*knappebredde)}px;">1</button>
+    <button class="kartknapper" style="height: ${knappehoyde}px; width: ${knappebredde}px; display: inline-block; position: absolute; top: ${bildehoyde-knappehoyde}px; left: ${(2*knappebredde)}px;">2</button>
+    <button class="kartknapper" style="height: ${knappehoyde}px; width: ${knappebredde}px; display: inline-block; position: absolute; top: ${bildehoyde-knappehoyde}px; left: ${(3*knappebredde)}px;">3</button>
     </div>
     `
     
