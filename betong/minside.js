cc1cb55cfe9723de976e3da8965d80b49ced1605
rubentreidene <55440579+rubentreidene@users.noreply.github.com>
2019-12-04 13:40:45 +0100

    minside

--- betong/minside.js
+++ betong/minside.js
@@ -4,7 +4,7 @@ function minside(innloggetbedrift) {
   html +=
   `
   <div style="background-color: #0B91E5; display: flex;">
-  <button onclick="mainHTML.innerHTML = minside(${innloggetbedrift})" style="display: inline-block; background-color: #0B91E5; border: none; font-size: 50px; color: white; width: 30px;">⤶</button>
+  <button onclick="mainHTML.innerHTML = kartside(${innloggetbedrift})" style="display: inline-block; background-color: #0B91E5; border: none; font-size: 50px; color: white; width: 30px;">⤶</button>
   <h2 style="width: 100%; display: inline-block; color: white;">Min Side</h2></br>
   </div>
     
