ade255f6dfe50d6e2c0ba745c1537bfe333a3735
martbry <martin_b91@hotmail.com>
2019-12-04 12:11:01 +0100

    Update kart.js

--- betong/kart.js
+++ betong/kart.js
@@ -3,17 +3,17 @@ let knappebredde = 90;
 let knappehoyde = 58;
 let bildehoyde = 640;
 
-function kartside(){
+function kartside(innloggetbedrift){
     let html = "";
 
     html += 
     `<div id="kartcontainer">
     <input type="text" style="position: absolute; width: 340px; margin-left: 10px;" value="Søk:"></input>
     <img src="kart.png" style="width: 360px; height: ${bildehoyde}px;"></img>
-    <button class="kartknapper" style="height: ${knappehoyde}px; width: ${knappebredde}px; display: inline-block; position: absolute; top: ${bildehoyde-knappehoyde}px; left: ${(0*knappebredde)}px;">0</button>
+    <button onclick="mainHTML.innerHTML = kartside(${innloggetbedrift})" class="kartknapper" style="height: ${knappehoyde}px; width: ${knappebredde}px; display: inline-block; position: absolute; top: ${bildehoyde-knappehoyde}px; left: ${(0*knappebredde)}px;">0</button>
     <button class="kartknapper" style="height: ${knappehoyde}px; width: ${knappebredde}px; display: inline-block; position: absolute; top: ${bildehoyde-knappehoyde}px; left: ${(1*knappebredde)}px;">1</button>
-    <button class="kartknapper" style="height: ${knappehoyde}px; width: ${knappebredde}px; display: inline-block; position: absolute; top: ${bildehoyde-knappehoyde}px; left: ${(2*knappebredde)}px;">2</button>
-    <button class="kartknapper" style="height: ${knappehoyde}px; width: ${knappebredde}px; display: inline-block; position: absolute; top: ${bildehoyde-knappehoyde}px; left: ${(3*knappebredde)}px;">3</button>
+    <button onclick="mainHTML.innerHTML = meldingsside(${innloggetbedrift})" class="kartknapper" style="height: ${knappehoyde}px; width: ${knappebredde}px; display: inline-block; position: absolute; top: ${bildehoyde-knappehoyde}px; left: ${(2*knappebredde)}px;">2</button>
+    <button onclick="mainHTML.innerHTML = minside(${innloggetbedrift})" class="kartknapper" style="height: ${knappehoyde}px; width: ${knappebredde}px; display: inline-block; position: absolute; top: ${bildehoyde-knappehoyde}px; left: ${(3*knappebredde)}px;">3</button>
     </div>
     `
     
