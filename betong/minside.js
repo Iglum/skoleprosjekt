8beb5c88ffd708256665b25f78303cd95f431300
rubentreidene <55440579+rubentreidene@users.noreply.github.com>
2019-12-04 13:24:46 +0100

    minside

--- betong/minside.js
+++ betong/minside.js
@@ -2,37 +2,37 @@ function minside(innloggetbedrift) {
   let html = "";
 
   html +=
-    "<button onclick='mainHTML.innerHTML = kartside(" +
-    innloggetbedrift +
-    ")' style='display: inline-block; background-color: #0B91E5; border: none; font-size: 25px; color: white; width: 30px;'>⤶</button>" +
-    "</br>" +
-    "Kontaktperson: " +
-    model.tilbyder[innloggetbedrift].kontaktperson +
-    "</br>" +
-    "Firmanavn: " +
-    model.tilbyder[innloggetbedrift].bedrift +
-    "</br>" +
-    "Adresse: " +
-    model.tilbyder[innloggetbedrift].adresse +
-    "</br>" +
-    "Orgnr: " +
-    model.tilbyder[innloggetbedrift].orgnr +
-    "</br>" +
-    "Telefon: " +
-    model.tilbyder[innloggetbedrift].tlf +
-    "</br>" +
-    "Email: " +
-    model.tilbyder[innloggetbedrift].email +
-    "</br>" +
-    "<button onclick='mainHTML.innerHTML = endreprofil(" + innloggetbedrift + ")'>Endre Profil</button>" +
-    " " +
-    "<button onclick='mainHTML.innerHTML = visHistorikk(" + innloggetbedrift + ")'>Historikk</button>" +
-    " " +
-    "<button>Sendte bud</button>" +
-    " " +
-    "<button class='kartknapper' style='height: 20px; width: 50px; display: inline-block; position: absolute; top: 620px; left: 0;'>0</button>" +
-    "<button class='kartknapper' style='height: 20px; width: 50px; display: inline-block; position: absolute; top: 620px; left: 50px;'>1</button>" +
-    "<button class='kartknapper' style='height: 20px; width: 50px; display: inline-block; position: absolute; top: 620px; left: 100px;'>2</button>" +
-    "<button class='kartknapper' style='height: 20px; width: 50px; display: inline-block; position: absolute; top: 620px; left: 150px;'>3</button>";
+  `
+    <button onclick='mainHTML.innerHTML = kartside(${innloggetbedrift})' style='display: inline-block; background-color: #0B91E5; border: none; font-size: 25px; color: white; width: 30px;'>⤶</button>
+    <h2 style='width: 100%; display: inline-block; color: white'>Min Side</h2>" 
+    
+    Kontaktperson: 
+    ${model.tilbyder[innloggetbedrift].kontaktperson}
+    </br> 
+    Firmanavn:  
+    ${model.tilbyder[innloggetbedrift].bedrift}
+    </br> 
+    Adresse:
+   ${model.tilbyder[innloggetbedrift].adresse}
+    </br>
+    Orgnr:  
+    ${model.tilbyder[innloggetbedrift].orgnr}
+    </br>
+    Telefon:
+    ${model.tilbyder[innloggetbedrift].tlf}
+    </br>
+    Email: 
+    ${model.tilbyder[innloggetbedrift].email}
+    </br>
+    <button onclick='mainHTML.innerHTML = endreprofil(${innloggetbedrift})'>Endre Profil</button>
+    
+    <button onclick='mainHTML.innerHTML = visHistorikk(${innloggetbedrift})'>Historikk</button>
+    <button>Sendte bud</button>
+    
+    <button class='kartknapper' style='height: 20px; width: 50px; display: inline-block; position: absolute; top: 620px; left: 0;'>0</button>
+    <button class='kartknapper' style='height: 20px; width: 50px; display: inline-block; position: absolute; top: 620px; left: 50px;'>1</button>
+    <button class='kartknapper' style='height: 20px; width: 50px; display: inline-block; position: absolute; top: 620px; left: 100px;'>2</button> 
+    <button class='kartknapper' style='height: 20px; width: 50px; display: inline-block; position: absolute; top: 620px; left: 150px;'>3</button>
+    `
   return html;
 }
