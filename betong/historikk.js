8455aa3fccdbcb3bc9cdedf747dacae13b0b6ce2
martbry <martin_b91@hotmail.com>
2019-12-04 10:55:14 +0100

    visHistorikk()
    Også lagt til funksjon for innloggetbedrift

--- betong/historikk.js
+++ betong/historikk.js
@@ -1,25 +1,34 @@
 
 
 
-function visHistorikk(){
+function visHistorikk(innloggetbedrift){
     let html = "";
-    let innloggetBedrift = 0;
 
-
+    html +=
+    `
+    <div style="background-color: #0B91E5; display: flex;">
+    <button onclick="mainHTML.innerHTML = minside(${innloggetbedrift})" style="display: inline-block; background-color: #0B91E5; border: none; font-size: 25px; color: white; width: 30px;">⤶</button>
+    <h2 style="width: 100%; display: inline-block; color: white;">Historikk</h2></br>
+    </div>
+    `
 
     for (let i = 0; i < model.historikk.length; i++){
-        if (model.historikk[i].bedrift == innloggetBedrift){
+        if (model.historikk[i].bedrift == innloggetbedrift){
             let historikk = model.historikk[i];
             for (let x = historikk.jobber.length-1; x >= 0; x--){
             
             let statusfarge = "";
             
-            statusfarge =historikk.jobber[x].status == "Fullført" ? statusfarge = "green" : statusfarge = "red";
+            statusfarge = historikk.jobber[x].status == "Fullført" ? statusfarge = "green" :
+                historikk.jobber[x].status == "Avslått" ? statusfarge = "red" : statusfarge = "grey";
             console.log(statusfarge);
+            
+            
             html +=
+            
             `
-            <div>
-                <h1>${historikk.jobber[x].tittel}</h1>
+            <div style="background-color: #F8F8F8; border: 1px solid #C6C6C6;">
+                <h3>${historikk.jobber[x].tittel}</h1>
                 <ul>
                 <li style="color: ${statusfarge}">${historikk.jobber[x].status}</li>
                 <li>${historikk.jobber[x].dato}</li>
@@ -27,7 +36,7 @@ function visHistorikk(){
                 </ul>
             </div>
             </br>
-            </br>
+            </div>
             `
             }
             
@@ -35,5 +44,6 @@ function visHistorikk(){
     }
     
     return html;
+
 
 }
