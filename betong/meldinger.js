496743c996a61e19b121fb4d912caf548aa81463
Iglum <33994566+Iglum@users.noreply.github.com>
2019-12-05 11:12:52 +0100

    :mega: Added meldinger

--- betong/meldinger.js
+++ betong/meldinger.js
@@ -1,17 +1,37 @@
 //meldinger
 
-function meldinger() {
+function meldinger(innloggetbedrift, person) {
   let html = "";
 
   html += `
-
-
-   
-    <p>${model.chat[0].personer[0].meldinger[0].innhold}</p>
-  
-         <div id="kartcontainer">
-    
-    
+    <div style="background-color: #0B91E5; display: flex;">
+    <button onclick="mainHTML.innerHTML = minside(${innloggetbedrift})" style="display: inline-block; background-color: #0B91E5; border: none; font-size: 50px; color: white; width: 30px;">⤶</button>
+    <h2 style="width: 100%; display: inline-block; color: white;">Tilbake</h2></br>
+    </div>
     `;
+
+  for (let i = 0; i < model.chat.length; i++) {
+    if (model.chat[i].bedrift == innloggetbedrift) {
+      let chat = model.chat[i];
+      for (let x = 0; x < chat.personer.length; x++) {
+        if (chat.personer[x].navn == person) {
+          let msg = chat.personer[x];
+          for (let y = 0; y < chat.personer[x].meldinger.length; y++) {
+            html += `
+            <div style="background-color: #F8F8F8; border: 1px solid #C6C6C6;">
+            <h4>${msg.meldinger[y].avsender}</h4>
+            <ul>
+            <li>${msg.meldinger[y].emne}</li>
+            <li>${msg.meldinger[y].innhold}</li>
+            </ul>
+            </div>
+            </br>
+            `;
+          }
+        }
+      }
+    }
+  }
+
   return html;
 }
