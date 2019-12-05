27fc10514482b35e023b9ade530d246921857808
rubentreidene <55440579+rubentreidene@users.noreply.github.com>
2019-12-05 10:19:27 +0100

    chat

--- betong/chatside.js
+++ betong/chatside.js
@@ -1,8 +1,35 @@
-function chatside(){
-    let hmtl = "";
+function chatside(innloggetbedrift){
+    let html = "";
+
+
+html += `
+<div style="background-color: #0B91E5; display: flex;">
+  
+  <h2 style="width: 100%; display: inline-block; color: white;">Chat</h2></br>
+  </div>
+
+${model.chat[0].personer[0].navn}
 
 
 
 
-    
+
+
+
+
+
+
+
+  <button onclick="mainHTML.innerHTML = kartside(${innloggetbedrift})" class="kartknapper" style="height: ${knappehoyde}px; width: ${knappebredde}px; display: inline-block; position: absolute; top: ${bildehoyde-knappehoyde}px; left: ${(0*knappebredde)}px;">${svgkartikon}</button>
+  <button class="kartknapper" style="height: ${knappehoyde}px; width: ${knappebredde}px; display: inline-block; position: absolute; top: ${bildehoyde-knappehoyde}px; left: ${(1*knappebredde)}px;">${svgsokikon}</button>
+  <button onclick="mainHTML.innerHTML = chatside(${innloggetbedrift})" class="kartknapper" style="height: ${knappehoyde}px; width: ${knappebredde}px; display: inline-block; position: absolute; top: ${bildehoyde-knappehoyde}px; left: ${(2*knappebredde)}px;">${svgmelding}</button>
+  <button onclick="mainHTML.innerHTML = minside(${innloggetbedrift})" class="kartknapper" style="height: ${knappehoyde}px; width: ${knappebredde}px; display: inline-block; position: absolute; top: ${bildehoyde-knappehoyde}px; left: ${(3*knappebredde)}px;">${svgminside}</button>
+
+
+
+
+
+`
+return html;
+
 }
