194b87d6967e0d8932fd443b3e3fc901f021ce3b
martbry <martin_b91@hotmail.com>
2019-12-06 13:18:34 +0100

    Navbar på chatside

--- betong/chatside.js
+++ betong/chatside.js
@@ -1,7 +1,7 @@
 function chatside(innloggetbedrift) {
-  let html = "";
+    let html = "";
 
-  html += `
+    html += `
 <div style="background-color: #0B91E5; display: flex;">
   
   <h2 style="width: 100%; display: inline-block; color: white;">Chat</h2></br>
@@ -14,8 +14,8 @@ function chatside(innloggetbedrift) {
                 <ul>
                 <li></span></li>
                 <li><h3>Emne:</h3> ${
-                  model.chat[0].personer[0].meldinger[0].emne
-                }</li>
+        model.chat[0].personer[0].meldinger[0].emne
+        }</li>
                 </br>
                
                 </ul>
@@ -27,8 +27,8 @@ function chatside(innloggetbedrift) {
                 <ul>
                 <li></span></li>
                 <li><h3>Emne:</h3> ${
-                  model.chat[0].personer[1].meldinger[0].emne
-                }</li>
+        model.chat[0].personer[1].meldinger[0].emne
+        }</li>
                 </br>
                
                 </ul>
@@ -40,29 +40,17 @@ function chatside(innloggetbedrift) {
                 <ul>
                 <li></span></li>
                 <li><h3>Emne:</h3> ${
-                  model.chat[0].personer[2].meldinger[0].emne
-                }</li>
+        model.chat[0].personer[2].meldinger[0].emne
+        }</li>
                 </br>
                
                 </ul>
             </div>
             </br>
             </div>
-
-
-  <button onclick="mainHTML.innerHTML = kartside(${innloggetbedrift})" class="kartknapper" style="height: ${knappehoyde}px; width: ${knappebredde}px; display: inline-block; position: absolute; top: ${bildehoyde -
-      knappehoyde}px; left: ${0 * knappebredde}px;">${model.svg[0].svg}</button>
-  <button class="kartknapper" style="height: ${knappehoyde}px; width: ${knappebredde}px; display: inline-block; position: absolute; top: ${bildehoyde -
-      knappehoyde}px; left: ${1 * knappebredde}px;">${model.svg[1].svg}</button>
-  <button onclick="mainHTML.innerHTML = chatside(${innloggetbedrift})" class="kartknapper" style="height: ${knappehoyde}px; width: ${knappebredde}px; display: inline-block; position: absolute; top: ${bildehoyde -
-      knappehoyde}px; left: ${2 * knappebredde}px;">${model.svg[2].svg}</button>
-  <button onclick="mainHTML.innerHTML = minside(${innloggetbedrift})" class="kartknapper" style="height: ${knappehoyde}px; width: ${knappebredde}px; display: inline-block; position: absolute; top: ${bildehoyde -
-      knappehoyde}px; left: ${3 * knappebredde}px;">${model.svg[3].svg}</button>
-
-
-
-
-
 `;
-  return html;
+
+    html += navbar;
+
+    return html;
 }
