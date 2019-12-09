4ad76223882b3a3573a027b1c3ab245d144adb55
Straytora <piaa@getacademy.no>
2019-12-09 12:58:55 +0100

    La inn dropdown til filtrering

--- betong/sokeside.js
+++ betong/sokeside.js
@@ -4,13 +4,24 @@ function sokeSide(innloggetBedrift) {
 
     html += `
     <div style="background-color: #0B91E5; display: flex;">
-    <h2 style="width: 100%; display: inline-block; color: white;">Søkeliste</h2></br></div>
+    <h2 style="width: 100%; display: inline-block; color: white; text-align: center;">Søkeliste</h2></br></div>
     <div><input type="text" width: 340px; margin-left: 10px; margin-top: 10px;" value="Søk:"></input></div>
     <div>
     <table>
     <tr>
-        <td>Område: Velg område</td>
-        <td>Sorter etter: Nyeste</td>
+        <td>Område: <select>
+            <option value="Holmestrand">Holmestrand</option>
+            <option value="Tønsberg">Tønsberg</option>
+            <option value="Sandefjord">Sandefjord</option>
+            <option value="Larvik">Larvik</option>
+            <option value="Oslo">Oslo</option>
+        </select></td>
+        <td>Sorter etter: <select>
+            <option value="Nyeste">Nyeste</option>
+            <option value="Eldste">Eldste</option>
+            <option value="Lavest">Lavest pris</option>
+            <option value="Høyest">Høyest pris</option>
+    </select></td>
     </tr>
     <tr>
         <td>
