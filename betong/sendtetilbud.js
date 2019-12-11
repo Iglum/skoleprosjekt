6546de4e92b480223865bb3f62c942eef78c9bd0
Straytora <piaa@getacademy.no>
2019-12-11 14:06:35 +0100

    Finpusset min side ++

--- betong/sendtetilbud.js
+++ betong/sendtetilbud.js
@@ -7,8 +7,8 @@ function sendteTilbud(innloggetbedrift){
     html +=
     `
     <div style="background-color: #0B91E5; display: flex;">
-    <button onclick="mainHTML.innerHTML = minside(${innloggetbedrift})" style="display: inline-block; background-color: #0B91E5; border: none; font-size: 50px; color: white; width: 30px;">⤶</button>
-    <h2 style="width: 100%; display: inline-block; color: white;">Sendte tilbud</h2></br>
+    <button onclick="mainHTML.innerHTML = minside(${innloggetbedrift})" style="display: inline-block; background-color: #0B91E5; border: none; font-size: 30px; color: white; padding-top: 5px; margin-left: 5px;">⤶</button>
+    <h2 style="width: 100%; display: inline-block; color: white; text-align: center; margin-right: 50px;">Sendte tilbud</h2></br>
     </div>
     `
 
@@ -20,9 +20,10 @@ function sendteTilbud(innloggetbedrift){
             html +=
             
             `
-            <div style="background-color: #F8F8F8; border: 1px solid #C6C6C6;">
+            </br>
+            <div style="background-color: #F8F8F8; border: 1px solid #C6C6C6; width: 90%; margin: auto; margin-bottom: -25px; padding-left: 10px;">
                 <h3>${sendt.jobber[x].tittel}</h1>
-                <ul>
+                <ul style="list-style-type:none;>
                 <li>Status: <span style="color: grey;">${sendt.jobber[x].status}</span></li>
                 <li>Dato: ${sendt.jobber[x].dato}</li>
                 </br>
