6dd6257320de69f14d485cf729ef699f64525aac
martbry <martin_b91@hotmail.com>
2019-12-16 11:18:24 +0100

    Registreringsgreier

--- betong/regbruker.js
+++ betong/regbruker.js
@@ -1,4 +1,4 @@
-function regBruker() {
+ï»¿function regBruker() {
 
     let html = "";
 
@@ -9,10 +9,10 @@ function regBruker() {
         <div style="background-color: #0B91E5; height: 30px"></div>
         <img src="https://www.betongsentrum.no/wp-content/uploads/2019/09/Bestillno3-1024x123.png" style="width: 360px; height: 50px;">
 
-        <div onclick="mainHTML = regKunde()" style="background-color: #F8F8F8; border: 1px solid #C6C6C6; width: 90%; margin: auto; margin-top: 100px; padding: 20px 10px 20px 10px;">
+        <div onclick="mainHTML.innerHTML = regKunde()" style="background-color: #F8F8F8; border: 1px solid #C6C6C6; width: 90%; margin: auto; margin-top: 100px; padding: 20px 10px 20px 10px;">
                 <h3>Registrer deg som kunde</h3>
                 <ul style="list-style-type:none;">
-                <li>Du trenger hjelp til betongarbeid eller snørydding</li>
+                <li>Du trenger hjelp til betongarbeid eller snÃ¸rydding</li>
                 </ul>
             </div>
             </br>
@@ -20,7 +20,7 @@ function regBruker() {
             <div onclick="mainHTML.innerHTML = regBedrift()" style="background-color: #F8F8F8; border: 1px solid #C6C6C6; width: 90%; margin: auto; padding: 20px 10px 20px 10px;">
                 <h3>Registrer deg som tilbyder</h3>
                 <ul style="list-style-type:none;">
-                <li>Du kan tilby tjenester innen betongarbeid eller snørydding</li>
+                <li>Du kan tilby tjenester innen betongarbeid eller snÃ¸rydding</li>
                 </ul>
             </div>
 
---
+++ betong/registrerkunde.js
@@ -0,0 +1,48 @@
+﻿
+function regKunde() {
+    let html = '';
+
+    html +=
+        `
+    <div id="registrerbruker">
+    <img src="https://www.betongsentrum.no/wp-content/uploads/2019/09/Bestillno3-1024x123.png" style="width: 360px; height: 50px;">
+    
+    <center>
+    </br>
+    </br>
+    <p><b>Registrer Bruker</b></p>
+    <table>
+    <tr>
+        <th>Ditt navn:</th> 
+        <th><input type="text"><th>
+    </tr>
+    <tr>
+        <th>Adresse:</th> 
+        <th><input type="text"><th>
+    </tr>
+    <tr>
+        <th>Epostadresse:</th> 
+        <th><input type="text"><th>
+    </tr>
+
+    <tr>
+        <th>Telefonnummer:</th> 
+        <th><input type="text"><th>
+    </tr>
+    </br>
+
+    </br>
+    </table>
+
+    <button onclick="mainHTML.innerHTML = loggInnSide()">Avbryt</button> <button onclick="mainHTML.innerHTML = kartside()">Fullfør</button>
+    
+    </div>
+
+
+    `
+
+    return html;
+
+}
+
+//<img src="svgbilbord.svg" alt="">
