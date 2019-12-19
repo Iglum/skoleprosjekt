6b0f8a076e187e66431b6fdf5683f9630c597f3d
rubentreidene <55440579+rubentreidene@users.noreply.github.com>
2019-12-19 09:37:18 +0100

    endrepassord

--- betong/endreprofil.js
+++ betong/endreprofil.js
@@ -1,5 +1,5 @@
-//endre profil-funksjonen gjør det samme som minside. Den sjekker hvilken bedrift som er innlogget og henter ut data 
-// fra modellen deretter. 
+//endre profil-funksjonen gjør det samme som minside. Den sjekker hvilken bedrift som er innlogget og henter ut data
+// fra modellen deretter.
 
 function endreprofil() {
   let html = "";
@@ -10,7 +10,7 @@ function endreprofil() {
 
   let person = "";
 
-  innlogget == "tilbyder" ? person = "Kontaktperson" : person = "Navn";
+  innlogget == "tilbyder" ? (person = "Kontaktperson") : (person = "Navn");
 
   html += `
     <div style="background-color: #0B91E5; display: flex;">
@@ -23,8 +23,10 @@ function endreprofil() {
     <div class="wrapper font">
     <center>
     ${person}:
-    <input id="endrenavn" type="text" name="${person}" value="${model.bruker[innloggetBruker][innlogget][person.toLowerCase()]}">
-    `
+    <input id="endrenavn" type="text" name="${person}" value="${
+    model.bruker[innloggetBruker][innlogget][person.toLowerCase()]
+  }">
+    `;
 
   if (innlogget == "tilbyder") {
     html += `
@@ -37,7 +39,7 @@ function endreprofil() {
     Adresse:
     <br>
     <input id="endreadresse" type="text" name="adresse" value="${model.bruker[innloggetBruker][innlogget].adresse}">
-    `
+    `;
 
   if (innlogget == "tilbyder") {
     html += `
@@ -47,7 +49,7 @@ function endreprofil() {
     Organisasjonsnr:
     <br>
     <input id="endreorgnr" type="text" name="orgnr" value="${model.bruker[innloggetBruker][innlogget].orgnr}">
-    `
+    `;
   }
 
   html += `
@@ -57,7 +59,21 @@ function endreprofil() {
       Email:
     <input id="endreemail" type="text" name="email" value="${model.bruker[innloggetBruker][innlogget].email}">
         <br>
+        Gammelt passord:
+        <br>
+        <input type="password" value="${model.bruker[innloggetBruker][innlogget].passord}" id="myInput">
+        
+<input type="checkbox" onclick="skjulPassord()">Vis Passord</input>
+<br>
+Nytt passord:
+<br>
+<input id="endrepassord" type"password" value=""></input>
+<br>
+
+           
 </center>
+<br>
+
 
 <center><div class="button-group" style="margin: auto;">
 <button onclick='lagreEndringer()'>Lagre</button>
@@ -67,16 +83,72 @@ function endreprofil() {
 
 </div>
 <br>
+<br>
+<br>
+<br>
 `;
 
-html += navbar;
+  html += navbar;
 
-return html;
+  return html;
+}
+
+function lagreEndringer() {
+  let navn = document.getElementById("endrenavn").value;
+  let adresse = document.getElementById("endreadresse").value;
+  let tlf = document.getElementById("endretlf").value;
+  let email = document.getElementById("endreemail").value;
+  let passord = document.getElementById("endrepassord").value;
+
+  if (innlogget == "tilbyder") {
+    let firmanavn = document.getElementById("endrebedrift").value;
+    let faktadresse = document.getElementById("endrefaktadresse").value;
+    let orgnr = document.getElementById("endreorgnr").value;
+
+    if (
+      navn != "" &&
+      adresse != "" &&
+      tlf != "" &&
+      email != "" &&
+      firmanavn != "" &&
+      faktadresse != "" &&
+      orgnr != ""
+    ) {
+      model.bruker[innloggetBruker][innlogget].kontaktperson = navn;
+      model.bruker[innloggetBruker][innlogget].adresse = adresse;
+      model.bruker[innloggetBruker][innlogget].tlf[0] = tlf;
+      model.bruker[innloggetBruker][innlogget].email = email;
+      model.bruker[innloggetBruker][innlogget].bedrift = firmanavn;
+      model.bruker[innloggetBruker][innlogget].faktadr = faktadresse;
+      model.bruker[innloggetBruker][innlogget].orgnr = orgnr;
+      model.bruker[innloggetBruker][innlogget].passord = passord;
+
+      mainHTML.innerHTML = minside();
+    }
+  } else if (innlogget == "kunde") {
+    if (navn != "" && adresse != "" && tlf != "" && email != "") {
+      model.bruker[innloggetBruker][innlogget].navn = navn;
+      model.bruker[innloggetBruker][innlogget].adresse = adresse;
+      model.bruker[innloggetBruker][innlogget].tlf[0] = tlf;
+      model.bruker[innloggetBruker][innlogget].email = email;
+      model.bruker[innloggetBruker][innlogget].passord = passord;
+
+      mainHTML.innerHTML = minside();
+    }
+  }
+}
+
+function skjulPassord() {
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
 }
 
 // <div class="btn-group" style="width:90%">
 //   <button onclick='mainHTML.innerHTML = visHistorikk(innloggetBedrift)' button style="width:50%">PLACEHOLDER TEXT</>
 //   <button onclick='mainHTML.innerHTML = visHistorikk(innloggetBedrift)' button style="width:50%">PLACEHOLDER TEXT</>
-// </div>  
+// </div>
 // <br>
-
